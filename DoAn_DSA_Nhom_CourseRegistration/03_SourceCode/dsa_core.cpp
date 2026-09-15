999e5d59a4270186e4bebcf052289af8c3a24916
VoDongPhuc <dphucdz45311@gmail.com>
2026-09-15 14:35:31 +0700

    tesst

--- DoAn_DSA_Nhom_CourseRegistration/03_SourceCode/dsa_core.cpp
+++ DoAn_DSA_Nhom_CourseRegistration/03_SourceCode/dsa_core.cpp
@@ -25,52 +25,7 @@ string dang_ky_mon(string ma_mon, string mssv) {
 
     // 3. Kiem tra da nam trong danh sach cho chua
     auto& hang_cho = ds_cho[ma_mon];
-    if (hang_cho.vi_tri_node.find(mssv) != hang_cho.vi_tri_node.end()) {
-        return "DA_NAM_TRONG_DANH_SACH_CHO";
-    }
-
-    HocPhan& hp = ds_hoc_phan[ma_mon];
-
-    // 4. Neu con cho -> cho vao danh sach chinh thuc
-    if (hp.si_so_hien_tai < hp.si_so_toi_da) {
-        hp.si_so_hien_tai++;
-        lop_chinh_thuc.push_back(mssv);
-        ghi_nhat_ky("DANG_KY", mssv, ma_mon);
-        return "THANH_CONG_CHINH_THUC";
-    }
-
-    // 5. Neu da het cho -> xep vao hang doi danh sach cho (FIFO)
-    hang_cho.hang_doi.push_back(mssv);
-    auto it = --hang_cho.hang_doi.end();
-    hang_cho.vi_tri_node[mssv] = it; // Luu iterator de xoa O(1) ve sau
-    ghi_nhat_ky("VAO_HANG_CHO", mssv, ma_mon);
-    return "THANH_CONG_VAO_HANG_CHO";
-}
-
-// MC2 (Trigger): Sinh vien huy mon chinh thuc
-// Neu co nguoi trong hang cho -> tu dong don nguoi nop som nhat len thay the
-string huy_mon_chinh_thuc(string ma_mon, string mssv) {
-    if (ds_hoc_phan.find(ma_mon) == ds_hoc_phan.end()) return "LOI_MON_KHONG_TON_TAI";
-
-    auto& lop_chinh_thuc = ds_chinh_thuc[ma_mon];
-    int vi_tri = -1;
-    for (int i = 0; i < (int)lop_chinh_thuc.size(); ++i) {
-        if (lop_chinh_thuc[i] == mssv) {
-            vi_tri = i;
-            break;
-        }
-    }
-
-    // Sinh vien khong co trong lop chinh thuc
-    if (vi_tri == -1) return "SINH_VIEN_KHONG_CO_TRONG_LOP";
-
-    // Xoa sinh vien khoi lop
-    lop_chinh_thuc.erase(lop_chinh_thuc.begin() + vi_tri);
-    ghi_nhat_ky("HUY_MON", mssv, ma_mon);
-
-    HocPhan& hp = ds_hoc_phan[ma_mon];
-    auto& hang_cho = ds_cho[ma_mon];
-
+    if (hang_cho.vi_tri
     // Kiem tra danh sach cho: neu co nguoi, lay nguoi dau tien vao lop (FIFO)
     if (!hang_cho.hang_doi.empty()) {
         string mssv_duoc_chon = hang_cho.hang_doi.front();
